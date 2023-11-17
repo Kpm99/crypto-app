b55ba13b2acf463b0236b47e9ad1d40624a31b06
Kpm99 <menonkrishna1208@gmail.com>
2023-11-17 16:53:55 +0530

    removed elements

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Header from './component/header'
 import Coins from './component/coins'
 import Exchanges from './component/Exchanges'
 import CoinDetails from './component/CoinDetails'
-import Home from './component/home'
+
 const App = () => {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/coins' element={<Coins/>}/>
+        <Route path='/' element={<Coins/>}/>
         <Route path='/exchanges' element={<Exchanges/>}/>
         <Route path='/coin/:id' element={<CoinDetails/>}/>
       </Routes>
